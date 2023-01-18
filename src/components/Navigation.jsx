464486437bf2f86f7ea3c9b1c8359c80b7d0072a
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-01-17 20:07:17 -0700

    update hover effects

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,8 +6,8 @@ const Navigation = () => {
             <a href="/" className="text-3xl">James-Ryan Stampley</a>
             <div className="p-5"></div>
             <ul className="flex justify-between items-center">
-                <li className="hover:bg-[#777] hover:text-lg hover:p-2 hover:ease-in hover:duration-300"><a className="" href="/about">About Me</a></li>
-                <li className="hover:bg-[#777] hover:text-lg hover:p-2 hover:ease-in hover:duration-300"><a className="" href="/contact">Contact Me</a></li>
+                <li className="hover:bg-[#777] hover:text-lg hover:p-2 hover:ease-in duration-300"><a className="" href="/about">About Me</a></li>
+                <li className="hover:bg-[#777] hover:text-lg hover:p-2 hover:ease-in duration-300"><a className="" href="/contact">Contact Me</a></li>
             </ul>
         </nav>
     )
