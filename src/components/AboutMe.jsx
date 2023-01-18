3b76ec2a29ef300a45a258c05844a8e95292ef9e
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-01-17 23:38:16 -0700

    add to AboutMe body section.

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,21 +4,18 @@ const AboutMe = () => {
     return(
         <div className="flex justify-center pt-10">
             <p className="text-xl tracking-wider bg-slate-200 p-10 rounded-md">
-                Hi there-- My name is James Stampley,<br/> 
-                a professional Software Engineer!<br/>
-                I specialize in using the Ruby Language<br/>
-                paired with the Ruby on Rails framework.<br/>
-                Ruby on Rails takes care of all my backend code.<br/>
-                For the front-end, I use JavaScript's React JS library<br/>
-                to serve up my front-end. Rails and React work well<br/>
-                together, and serve as my main Technical Stack for<br/>
-                bringing projects and applications to life. Creating<br/>
-                applications and coding in generally are both very important<br/>
-                to me. So much so, that even when i'm not working<br/>
-                professionally, i'm still learning new concepts and<br/>
-                applying them to my own personal projects. I'm always up<br/>
-                for a challenge and learning new things, so I think<br/>
-                I've landed in the right career path for me. 
+                Hi there-- My name is James Stampley, a professional Software Engineer!<br/> 
+                I specialize in using the Ruby Language paired with the Ruby on Rails framework.<br/>
+                Ruby on Rails takes care of all my backend code. For the front-end, I use JavaScript's<br/>
+                React JS library to serve up my front-end. Rails and React work well together,<br/>
+                and serve as my main Technical Stack for bringing projects and applications to life.<br/>
+                I have a niche specialty in
+                web-scraping using Ruby's Capybara, Selenium and Nokogiri.<br/>                                             
+                My Python skills also come in handy for web-scraping or automation tasks!<br/>
+                Creating applications and the craft of coding are both very important to me. <br/>
+                So much so, that even when i'm not working professionally, i'm still learning new<br/>
+                concepts and applying them to my own personal projects. I'm always up<br/>
+                for a challenge and learning new things, so I think I've landed in the right career path for me. 
             </p>
         </div>
     )
