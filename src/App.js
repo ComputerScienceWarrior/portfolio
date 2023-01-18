d40d43d42f5a3d3f3bdf79917ade412f6d064d87
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-01-17 19:52:26 -0700

    wrap App component in react-router-dom-v6

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 import Home from './components/Home';
+import ContactMe from './components/ContactMe'
+import AboutMe from './components/AboutMe'
+import Error from './components/Error';
+import Navigation from './components/Navigation';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutMe />} />
+        <Route path="/contact" element={<ContactMe />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
   );
 }
 
