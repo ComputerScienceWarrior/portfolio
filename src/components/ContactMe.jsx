619908e04db64ff390e1de4c05d7140474659e6b
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-01-18 22:17:03 -0700

    create a boilerplate contact-me form

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -2,8 +2,20 @@ import React from "react";
 
 const ContactMe = () => {
     return(
-        <div>
-            Contact Me.
+        <div className="p-5 flex justify-center">
+            <form className="">
+                <label>Reason For Contact: </label><br/>
+                <input className="border-2 solid border-black rounded w-full" placeholder="**Enter a subject title**" /><br />
+                
+                <label>Body of Post: </label><br/>
+                <textarea className="border-2 solid border-black rounded w-full"/><br/>
+                <label>Contact Method:</label>
+                <select>
+                    <option value={1}></option>
+                    <option value={2}></option>
+                </select>
+                <button type="submit" className="border-2 solid border-black rounded w-full bg-gray-300 text-lg">Submit</button>
+            </form>
         </div>
     )
 };
