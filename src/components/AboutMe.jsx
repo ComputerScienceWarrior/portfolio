13d64a27087e983fc5f29502518a32fc40ca67a1
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-01-18 22:16:34 -0700

    fix formatting of paragraph text

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,15 +2,14 @@ import React from "react";
 
 const AboutMe = () => {
     return(
-        <div className="flex justify-center pt-10">
-            <p className="text-xl tracking-wider bg-slate-200 p-10 rounded-md">
+        <div className="flex justify-center pt-10 scroll-auto">
+            <p className="text-xl tracking-wider bg-slate-200 p-10 rounded-md min-h-screen">
                 Hi there-- My name is James Stampley, a professional Software Engineer!<br/> 
                 I specialize in using the Ruby Language paired with the Ruby on Rails framework.<br/>
                 Ruby on Rails takes care of all my backend code. For the front-end, I use JavaScript's<br/>
                 React JS library to serve up my front-end. Rails and React work well together,<br/>
                 and serve as my main Technical Stack for bringing projects and applications to life.<br/>
-                I have a niche specialty in
-                web-scraping using Ruby's Capybara, Selenium and Nokogiri.<br/>                                             
+                I have a niche specialty in web-scraping using Ruby's Capybara, Selenium and Nokogiri.<br/>                                             
                 My Python skills also come in handy for web-scraping or automation tasks!<br/>
                 Creating applications and the craft of coding are both very important to me. <br/>
                 So much so, that even when i'm not working professionally, i'm still learning new<br/>
