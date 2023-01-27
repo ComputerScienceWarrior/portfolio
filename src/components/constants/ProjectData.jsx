1f646c79cdf652704bcd6c8d50814bdc6b034735
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-01-27 15:16:29 -0700

    create projectData to render

---
+++ src/components/constants/ProjectData.jsx
@@ -0,0 +1,14 @@
+export const ProjectData = [
+    { title: 'Project 1', 
+      caption: 'This is Project 1\'s caption.', 
+      githubUrl: 'https://github.com/ComputerScienceWarrior/job_search' 
+    },
+    { title: 'Project 2', 
+      caption: 'This is Project 2\'s caption.', 
+      githubUrl: 'https://github.com/ComputerScienceWarrior/InventoryManagementSystem_v2' 
+    },
+    { title: 'Project 3', 
+      caption: 'This is Project 3\'s caption.', 
+      githubUrl: 'https://github.com/ComputerScienceWarrior/buy-low-sell-high' 
+    }
+]
