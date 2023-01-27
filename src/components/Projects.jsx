4a507c84f093d6b3752e20d54e04a93266095a0f
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-01-27 15:16:47 -0700

    map over and display all project from ProjectData const.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Project from "./Project";
+import { ProjectData } from "./constants/ProjectData";
 
 const Projects = () => {
     return(
-        <div>
-            Projects:
-            <Project />
-        </div>
+        <>
+            <h1 className="text-center text-2xl font-bold italic">My Projects</h1>
+            <div className="flex justify-evenly">
+                {ProjectData.map(project => {
+                    return <Project title={project.title} caption={project.caption} />
+                })}
+            </div>
+        </>
     )
 }
 
