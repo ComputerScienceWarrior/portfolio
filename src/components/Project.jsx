cae7081216d2c71fb5273f04e2049497c733f4d3
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-01-31 18:53:47 -0700

    display project with on-hover effect.

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Project = (props) => {
     return(
         <div>
-            <h1 className="text-center font-bold">{props.title}</h1>
-            <div className="w-48 h-48 border-2 border-black rounded bg-gray-300">
-                {/* image thumbnail will go here. */}
+            <h1 className="text-center font-bold text-2xl">{props.title}</h1>
+            <div className="w-48 h-48 hover:opacity-25 border-2 border-black rounded bg-gray-300 duration-300">
+                <img src={props.imageUrl} className="w-full h-full" alt="Cat Picture."/>
             </div>
             <p className="text-center">{props.caption}</p>
         </div>
