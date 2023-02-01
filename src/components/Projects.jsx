9d0a8959bcdcee71953d5a77a7ede8fb7181b7ee
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-01-31 18:52:53 -0700

    display image in component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,8 +7,8 @@ const Projects = () => {
         <>
             <h1 className="text-center text-2xl font-bold italic">My Projects</h1>
             <div className="flex justify-evenly">
-                {ProjectData.map(project => {
-                    return <Project title={project.title} caption={project.caption} />
+                {ProjectData.map((project, key) => {
+                    return <Project key={key} title={project.title} caption={project.caption} imageUrl={project.imageUrl} />
                 })}
             </div>
         </>
