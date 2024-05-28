0bc2e0c8bb2b65be3d89d6a39979ec7a360b9bcd
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-05-28 14:47:13 -0700

    adjusts structure of Home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,21 +3,25 @@ import React from "react";
 
 const Home = () => {
     return (
-        <Box className="flex-col justify-center">
-            <Text className="text-4xl text-center text-green-700 italic underline">
-                Welcome to my Page!
-            </Text>
-            <Text className="space-x-4">
-                <Button colorScheme="blue" size={'lg'} >
-                    Portfolio Projects
-                </Button>
-                <Button colorScheme="green" size={'lg'} >
-                    Resume
-                </Button>
-                <Button colorScheme="red" size={'lg'} >
-                    About Me
-                </Button>
-            </Text>
+        <Box>
+            <Box>
+                <Text className="text-4xl text-center text-white italic underline">
+                    Welcome to my Page!
+                </Text>
+            </Box>
+            <Box className="flex-col justify-center mt-64 mb-64">
+                <Text className="space-x-4">
+                    <Button colorScheme="blue" size={'lg'} >
+                        Portfolio Projects
+                    </Button>
+                    <Button colorScheme="green" size={'lg'} >
+                        Resume
+                    </Button>
+                    <Button colorScheme="red" size={'lg'} >
+                        About Me
+                    </Button>
+                </Text>
+            </Box>
         </Box>
     )
 }
