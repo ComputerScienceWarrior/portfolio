178d5bfc07c06ac08343f7a3fcc634cf8cbe28e4
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-05-28 14:47:57 -0700

    adds blue to background

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
-    <ChakraProvider>
-      <Box className='flex justify-center flex-col items-center pt-10'>
+    <ChakraProvider className="bg-blue-900">
+      <Box className='flex justify-center flex-col items-center pt-10 bg-blue-900'>
         <Home />
       </Box>
     </ChakraProvider>
