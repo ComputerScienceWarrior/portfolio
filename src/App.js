5b3efb2fe9507c90588d4b0f9a216c2401d4ca1d
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-05-28 13:28:13 -0700

    sets up tailwind css and chakra ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import * as React from 'react'
+import { Box } from '@chakra-ui/react';
 
 import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
     <ChakraProvider>
-      <Box>
+      <Box className='text-center text-red-500'>
         Hello World... with Chakra UI.
       </Box>
     </ChakraProvider>
