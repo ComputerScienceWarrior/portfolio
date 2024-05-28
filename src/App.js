fb66ed8609800358ddd8c934a3dd9dd01beff2a5
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-05-28 14:27:48 -0700

    imports and centers 'Home' component link

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import * as React from 'react'
 import { Box } from '@chakra-ui/react';
+import Home from './components/Home/Home';
 
 import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
     <ChakraProvider>
-      <Box className='text-center text-red-500'>
-        Hello World... with Chakra UI.
+      <Box className='flex justify-center flex-col items-center pt-10'>
+        <Home />
       </Box>
     </ChakraProvider>
   )
