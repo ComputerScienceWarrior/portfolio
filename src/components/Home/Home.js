faed89f34dd5298ca226a4480860d8ec647c3769
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-05-28 20:42:00 -0700

    adds logic for Buttons routing to specific component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,29 +1,48 @@
 import { Box, Button, Text } from "@chakra-ui/react";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+    const navigate = useNavigate();
+
+    const handleNavigation = (path) => {
+        navigate(path);
+    };
+
     return (
         <Box>
             <Box>
-                <Text className="text-4xl text-center text-white italic underline">
-                    Welcome to my Page!
+                <Text fontFamily={'cursive'} className="text-4xl text-center text-white italic">
+                    James-Ryan Stampley
                 </Text>
             </Box>
             <Box className="flex-col justify-center mt-64 mb-64">
                 <Text className="space-x-4">
-                    <Button colorScheme="blue" size={'lg'} >
+                    <Button
+                        colorScheme="blue"
+                        size={'lg'}
+                        onClick={() => handleNavigation('/portfolios')}
+                    >
                         Portfolio Projects
                     </Button>
-                    <Button colorScheme="green" size={'lg'} >
+                    <Button
+                        colorScheme="green"
+                        size={'lg'}
+                        onClick={() => handleNavigation('/resume')}
+                    >
                         Resume
                     </Button>
-                    <Button colorScheme="red" size={'lg'} >
+                    <Button
+                        colorScheme="red"
+                        size={'lg'}
+                        onClick={() => handleNavigation('/about')}
+                    >
                         About Me
                     </Button>
                 </Text>
             </Box>
         </Box>
-    )
-}
+    );
+};
 
 export default Home;
