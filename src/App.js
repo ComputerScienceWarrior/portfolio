9e4e997824e2c99769e73812553501a7409b9ac4
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-05-28 20:40:27 -0700

    adds routes for various components (Resume, About, etc.)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import * as React from 'react'
-import { Box } from '@chakra-ui/react';
+import * as React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Box, ChakraProvider } from '@chakra-ui/react';
 import Home from './components/Home/Home';
-
-import { ChakraProvider } from '@chakra-ui/react'
+import Portfolios from './components/Portfolios/Portfolios';
+import Resume from './components/Resume/Resume';
+import About from './components/About/About';
 
 function App() {
   return (
-    <ChakraProvider className="bg-blue-900">
-      <Box className='flex justify-center flex-col items-center pt-10 bg-blue-900'>
-        <Home />
-      </Box>
+    <ChakraProvider>
+      <Router>
+        <Box 
+          className='flex justify-center flex-col items-center pt-10 bg-blue-900' 
+          minH="100vh" 
+          w="100vw"
+        >
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/portfolios" element={<Portfolios />} />
+            <Route path="/resume" element={<Resume />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Box>
+      </Router>
     </ChakraProvider>
-  )
+  );
 }
 
 export default App;
