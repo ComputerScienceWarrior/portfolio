6cddf0e3bbc0b1ccfcad24d5c0b4219b9f1575c0
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-05-28 20:42:17 -0700

    creates initial 'Resume' component

---
+++ src/components/Resume/Resume.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box } from "@chakra-ui/react";
+
+const Resume = () => {
+    return (
+        <Box>
+            My Resume!
+        </Box>
+    )
+}
+
+export default Resume;
